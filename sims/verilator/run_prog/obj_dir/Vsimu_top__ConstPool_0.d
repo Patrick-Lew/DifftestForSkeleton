40b9ad2106e3e109da419b8ff90a97aa318fd094
Yan Pan <ypan2003@yeah.net>
2024-05-09 21:13:16 +0800

    [MISC]: Remove track of auto generated files

--- sims/verilator/run_prog/obj_dir/Vsimu_top__ConstPool_0.d
+++
@@ -1,5 +0,0 @@
-Vsimu_top__ConstPool_0.o: Vsimu_top__ConstPool_0.cpp \
- /media/creeperrush/Sandbox/verilator/include/verilated.h \
- /media/creeperrush/Sandbox/verilator/include/verilatedos.h \
- /media/creeperrush/Sandbox/verilator/include/verilated_types.h \
- /media/creeperrush/Sandbox/verilator/include/verilated_funcs.h
--- sims/verilator/run_prog/obj_dir/Vsimu_top__Dpi.d
+++
@@ -1,8 +0,0 @@
-Vsimu_top__Dpi.o: Vsimu_top__Dpi.cpp Vsimu_top__Dpi.h \
- /media/creeperrush/Sandbox/verilator/include/vltstd/svdpi.h Vsimu_top.h \
- /media/creeperrush/Sandbox/verilator/include/verilated.h \
- /media/creeperrush/Sandbox/verilator/include/verilatedos.h \
- /media/creeperrush/Sandbox/verilator/include/verilated_types.h \
- /media/creeperrush/Sandbox/verilator/include/verilated_funcs.h \
- /media/creeperrush/Sandbox/verilator/include/verilated_save.h \
- /media/creeperrush/Sandbox/verilator/include/verilated.h
